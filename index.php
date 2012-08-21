051de640bff3fe19efb6a287c40e4395bf83f1c4
Katy Ereira <katy.ereira@gmail.com>
2012-08-21 20:37:19 +0100

    Pedantry

--- index.php
+++ index.php
@@ -2,59 +2,55 @@
 	function hello_world($language = FALSE)
 	{	
 		$hello = array(
-			'English' => 'Hello',
-			'Japanese' => 'Konnichiwa',
-			'French' => 'Bonjour',
-			'German' => 'Hallo',
-			'Italian' => 'Ciao',
-			'Spanish' => 'Hola',
+			'English'          => 'Hello',
+			'Japanese'         => 'Konnichiwa',
+			'French'           => 'Bonjour',
+			'German'           => 'Hallo',
+			'Italian'          => 'Ciao',
+			'Spanish'          => 'Hola',
 			'Mandarin Chinese' => 'Ni Hao',
-			'Danish' => 'Hej',
-			'Malay' => 'Selamat pagi',
-			'Maori' => 'Kia ora',
-			'Hawaiian' => 'Aloha',
-			'Hindi' => 'Namaste',
-			'Arabic' => "As-salaam 'alaikum"
+			'Danish'           => 'Hej',
+			'Malay'            => 'Selamat pagi',
+			'Maori'            => 'Kia ora',
+			'Hawaiian'         => 'Aloha',
+			'Hindi'            => 'Namaste',
+			'Arabic'           => "As-salaam 'alaikum",
 			);
 			
 		$goodbye = array(
-			'English' => 'Goodbye',
-			'Japanese' => 'Sayounara',
-			'French' => 'Au reviour',
-			'German' => 'Auf wiedersehen',
-			'Italian' => 'Arrivederci',
-			'Spanish' => 'Adios',
+			'English'          => 'Goodbye',
+			'Japanese'         => 'Sayounara',
+			'French'           => 'Au reviour',
+			'German'           => 'Auf wiedersehen',
+			'Italian'          => 'Arrivederci',
+			'Spanish'          => 'Adios',
 			'Mandarin Chinese' => 'Baibai',
-			'Danish' => 'Farvel',
-			'Malay' => 'Selamat tinggal',
-			'Maori' => 'E noho raa',
-			'Hawaiian' => 'A hui ho',
-			'Hindi' => 'Namaste',
-			'Arabic' => "Ilaa al-liqaa"
+			'Danish'           => 'Farvel',
+			'Malay'            => 'Selamat tinggal',
+			'Maori'            => 'E noho raa',
+			'Hawaiian'         => 'A hui ho',
+			'Hindi'            => 'Namaste',
+			'Arabic'           => 'Ilaa al-liqaa',
 			);
 
-		if ($language AND isset($hello[$language]))
-		{
-			$use_language = $language;
-		}
-		else if ($language)
+		if ($language AND ! isset($hello[$language]))
 		{
 			return "Sorry, I don't know the $language language!";
 		}
-		else
+		else if ( ! $language)
 		{
-			$use_language = array_rand($hello);
+			$language = array_rand($hello);
 		}
 		
-		return "$hello[$use_language] world! $goodbye[$use_language] world! (That was $use_language!)";
+		return "$hello[$language] world! $goodbye[$language] world! (That was $language!)";
 	}
 	
 	// A language I know
-	echo hello_world('Hindi').'<br />';
+	echo hello_world('Hindi'), '<br />', PHP_EOL;
 	
 	// A language I don't know
-	echo hello_world('Norwegian').'<br />';
+	echo hello_world('Norwegian'), '<br />', PHP_EOL;
 	
 	// A random language
-	echo hello_world().'<br />';
+	echo hello_world(), '<br />';
 ?>
