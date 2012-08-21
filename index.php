342fd9803cc30c183150aca1b8ef42c50339a2cd
Katy Ereira <katy.ereira@gmail.com>
2012-08-21 19:29:09 +0100

    Additional languages

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-	echo "Hello world!";
+	$languages = array(
+		'Japanese' => 'Konnichiwa',
+		'French' => 'Bonjour',
+		'German' => 'Hallo',
+		'Italian' => 'Ciao',
+		'Spanish' => 'Hola',
+		'Mandarin Chinese' => 'Ni Hao',
+		'Danish' => 'Hej',
+		'Malay' => 'Selamat pagi',
+		'Maori' => 'Kia ora',
+		'Hawaiian' => 'Aloha',
+		'Hindi' => 'Namaste',
+		'Arabic' => "As-salaam 'alaikum"
+		);
+
+	$random_language = array_rand($languages);
+	
+	echo "$languages[$random_language] world! (That was $random_language!)";
 ?>
