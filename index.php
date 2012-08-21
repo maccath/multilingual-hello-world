fb3d617ab99bca8135f5c3c461093d8183d981b9
Katy Ereira <katy.ereira@gmail.com>
2012-08-21 19:38:42 +0100

    Turned it into a function

--- index.php
+++ index.php
@@ -1,20 +1,43 @@
 <?php
-	$languages = array(
-		'Japanese' => 'Konnichiwa',
-		'French' => 'Bonjour',
-		'German' => 'Hallo',
-		'Italian' => 'Ciao',
-		'Spanish' => 'Hola',
-		'Mandarin Chinese' => 'Ni Hao',
-		'Danish' => 'Hej',
-		'Malay' => 'Selamat pagi',
-		'Maori' => 'Kia ora',
-		'Hawaiian' => 'Aloha',
-		'Hindi' => 'Namaste',
-		'Arabic' => "As-salaam 'alaikum"
-		);
+	function hello_world($language = FALSE)
+	{	
+		$languages = array(
+			'Japanese' => 'Konnichiwa',
+			'French' => 'Bonjour',
+			'German' => 'Hallo',
+			'Italian' => 'Ciao',
+			'Spanish' => 'Hola',
+			'Mandarin Chinese' => 'Ni Hao',
+			'Danish' => 'Hej',
+			'Malay' => 'Selamat pagi',
+			'Maori' => 'Kia ora',
+			'Hawaiian' => 'Aloha',
+			'Hindi' => 'Namaste',
+			'Arabic' => "As-salaam 'alaikum"
+			);
 
-	$random_language = array_rand($languages);
+		if ($language AND isset($languages[$language]))
+		{
+			$use_language = $language;
+		}
+		else if ($language)
+		{
+			return "Sorry, I don't know the $language language!";
+		}
+		else
+		{
+			$use_language = array_rand($languages);
+		}
+		
+		return "$languages[$use_language] world! (That was $use_language!)";
+	}
 	
-	echo "$languages[$random_language] world! (That was $random_language!)";
+	// A language I know
+	echo hello_world('Hindi').'<br />';
+	
+	// A language I don't know
+	echo hello_world('Norwegian').'<br />';
+	
+	// A random language
+	echo hello_world().'<br />';
 ?>
